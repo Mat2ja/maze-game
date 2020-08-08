f7784cb65a0073ee262809f30add42f9c171e13c
Matija Osrecki <matija.osrecki@gmail.com>
2020-08-08 19:48:09 +0200

    built maze wall algorythm :wrench:

---
+++ alt.js
@@ -0,0 +1,4 @@
+//* Maze generation 🧬
+// Bad solution bsc if we modify one, it modifies all
+// i.e grid[0].push(true)) changes all 3 rows
+// const grid = Array(3).fill([false,false,false]
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-gridconsole.clear();
+console.clear();
 
 // module aliases
-
 const {
     Engine,
     Render,
@@ -10,9 +9,11 @@ const {
     Bodies,
 } = Matter;
 
-const cells = 5;
+const cells = 3;
 const width = 600;
 const height = 600;
+
+const unitLength = width / cells;
 
 // create an engine
 const engine = Engine.create();
@@ -25,7 +26,7 @@ const render = Render.create({
     element: document.body,
     engine: engine,
     options: {
-        wireframes: true,
+        wireframes: false,
         width,
         height
     }
@@ -47,15 +48,35 @@ const walls = [
 ];
 World.add(world, walls);
 
+//* Building a maze🐭
+//1. generate the maze
+//2. pick a random starting cell
+//3. for that cell, build a randomly-ordered list of neighbours
+//4. if a neighbour has been visited before, remove it from the list
+//5. for each remaining neighbour, 'move' to it and remove the wall between those two cells
+//6. repeat for new neighbour (step 3)
+
 //* Maze generation 🧬
-// Bad solution bsc if we modify one, it modifies all
-// i.e grid[0].push(true)) changes all 3 rows
-// const grid = Array(3).fill([false,false,false]
+
+// Shuffle neighbours
+const shuffle = (arr) => {
+    let counter = arr.length;
+
+    while (counter > 0) {
+        const index = Math.floor(Math.random() * counter);
+        counter--;
+
+        const temp = arr[counter];
+        arr[counter] = arr[index];
+        arr[index] = temp;
+
+    };
+    return arr;
+}
 
 const grid = Array(cells)
     .fill(null)
     .map(() => Array(cells).fill(false));
-
 
 const verticals = Array(cells)
     .fill(null)
@@ -65,8 +86,110 @@ const horizontals = Array(cells - 1)
     .fill(null)
     .map(() => Array(cells).fill(false));
 
-// console.log(grid);
-// console.log(verticals);
-// console.log(horizontals);
-
 // Pick a random starting cell
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+console.log(startRow, startColumn);
+
+const stepThroughCell = (row, column) => {
+    // If I have visited the cell at [row,column], then return
+    if (grid[row][column]) {
+        return;
+    }
+
+    // Mark that cell as being visited
+    grid[row][column] = true;
+
+    // Assemble randomly-ordered list of neighbours
+    //* [00,01,02]
+    //* [10,11,12]
+    //* [20,21,22]
+
+    const neighbours = shuffle([
+        [row - 1, column, 'up'], // above
+        [row, column + 1, 'right'], // right
+        [row + 1, column, 'down'], // below
+        [row, column - 1, 'left'], // left
+    ]);
+
+    // For each neighbour...
+    for (let neighbour of neighbours) {
+        const [nextRow, nextColumn, direction] = neighbour;
+
+        // See if neighbour is out of bounds
+        if (nextRow < 0 || nextRow >= cells || nextColumn < 0 || nextColumn >= cells) {
+            continue; // move onto next iteration/neighbour
+        };
+
+        // If we have visited that neighbour, continue to next neighbour
+        if (grid[nextRow][nextColumn]) {
+            continue;
+        };
+
+        // Remove a wall from either verticals or horizontals
+        if (direction === 'left') {
+            verticals[row][column - 1] = true;
+        } else if (direction === 'right') {
+            verticals[row][column] = true;
+        } else if (direction === 'up') {
+            horizontals[row - 1][column] = true;
+        } else if (direction === 'down') {
+            horizontals[row][column] = true;
+        }
+
+        stepThroughCell(nextRow, nextColumn);
+    };
+};
+
+
+stepThroughCell(startRow, startColumn);
+console.log('grid', grid);
+console.log('verticals', verticals);
+console.log('horizontals', horizontals);
+
+horizontals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        };
+
+        const unitLength = width / cells;
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength / 2,
+            rowIndex * unitLength + unitLength,
+            unitLength,
+            10,
+            {
+                isStatic: true,
+                render: { fillStyle: 'orange' }
+            });
+
+        World.add(world, wall);
+    })
+});
+
+verticals.forEach((row, rowIndex) => {
+    row.forEach((open, columnIndex) => {
+        if (open) {
+            return;
+        };
+
+        const unitLength = width / cells;
+
+        const wall = Bodies.rectangle(
+            columnIndex * unitLength + unitLength,
+            rowIndex * unitLength + unitLength / 2,
+            10,
+            unitLength,
+            {
+                isStatic: true,
+                render: { fillStyle: 'orange' }
+            });
+
+        World.add(world, wall);
+    })
+});
+
+
+
